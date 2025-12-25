07fbb099bd4328a979bf889c24807c344b839f77
kio06 <clintjohnmalasa.bonagua24@bicol-u.edu.ph>
2025-12-25 19:16:35 +0800

    Config: Integrate task routes into main server

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+const taskRoutes = require('./routes/taskRoutes');
+
 const app = express();
 
 // Middleware
@@ -15,9 +17,12 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log('DB Error:', err));
 
 // Routes
-app.get('/', (req, res) => {
+app.get('/', (req, res) => 
+{
   res.send('API is running...');
 });
+
+app.use('/api/tasks', taskRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
